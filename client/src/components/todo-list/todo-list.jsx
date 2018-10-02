e53b292608030548a2126cf6593f484c3275cdbe
Anton Punko <ant.punko@gmail.com>
2018-10-02 18:44:06 +0300

    add mock provider

--- client/src/components/todo-list/todo-list.jsx
+++ client/src/components/todo-list/todo-list.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Query } from 'react-apollo';
 import TodoItem from '../todo-item';
+import { GET_TODO_LIST } from '../../queries';
 
-const TodoList = ({ title, items }) => {
-  const todoItems = items.map(item => <TodoItem key={item.id} title={item.title} />);
+const TodoList = ({ id }) => (
+  <Query query={GET_TODO_LIST} variables={{ id }}>
+    {({ loading, error, data }) => {
+      if (loading) return null;
+      if (error) return `Error!: ${error}`;
 
-  return (
-    <div>
-      <h1>{title}</h1>
-      {todoItems}
-    </div>
-  );
-};
+      const { title, items } = data.todoList;
+      const todoItems = items.map(item => <TodoItem key={item.id} title={item.title} />);
+
+      return (
+        <div>
+          <h1>{title}</h1>
+          {todoItems}
+        </div>
+      );
+    }}
+  </Query>
+);
 
 TodoList.propTypes = {
-  title: PropTypes.string.isRequired,
-  items: PropTypes.PropTypes.arrayOf(PropTypes.object).isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default TodoList;
