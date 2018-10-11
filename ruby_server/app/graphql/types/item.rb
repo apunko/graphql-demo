736cec4a639a25dc5c3e72daa1af21965c02ee9d
Anton Punko <ant.punko@gmail.com>
2018-10-11 14:52:22 +0300

    remove obsolete ruby_server

--- ruby_server/app/graphql/types/item.rb
+++
@@ -1,9 +0,0 @@
-Types::Item = GraphQL::ObjectType.define do
-  name 'Item'
-
-  field :id, types.Int
-  field :title, types.String
-  field :description, types.String
-  field :updatedAt, Types::DateTime, property: :updated_at
-  field :createdAt, Types::DateTime, property: :created_at
-end
--- ruby_server/app/graphql/types/list.rb
+++
@@ -1,9 +0,0 @@
-Types::List = GraphQL::ObjectType.define do
-  name 'List'
-
-  field :id, types.Int
-  field :title, types.String
-  field :updatedAt, Types::DateTime, property: :updated_at
-  field :createdAt, Types::DateTime, property: :created_at
-  field :todoItems, types[Types::Item], property: :items
-end
