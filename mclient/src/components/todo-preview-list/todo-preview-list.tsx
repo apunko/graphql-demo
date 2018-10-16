979a8d531ef44bbbb7d97e5b039ac5cff6c0815f
Anton Punko <ant.punko@gmail.com>
2018-10-16 13:44:05 +0300

    query todo lists preview

--- mclient/src/components/todo-preview-list/todo-preview-list.tsx
+++ mclient/src/components/todo-preview-list/todo-preview-list.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { Query } from 'react-apollo';
 import TodoListPreview from '../todo-list-preview';
+import { GET_ALL_TODOS } from '../../queries';
 
 interface TodoPreviewListProps {
   handleSelect(id: number, title: string): void;
 }
 
-const TodoPreviewList = ({ handleSelect }: TodoPreviewListProps) => {
-  const todos = [{ id: 3, title: '123' }, { id: 5, title: '123' }];
-  const todoListPreviews = todos.map((list) => (
-    <TodoListPreview
-      key={list.id}
-      id={list.id}
-      title={list.title}
-      handleSelect={handleSelect}
-    />
-  ));
+const TodoPreviewList = ({ handleSelect }: TodoPreviewListProps) => (
+  <Query query={GET_ALL_TODOS}>
+    {({ loading, error, data }) => {
+      if (loading) { return <Text>Loading todos...</Text>; }
+      if (error) { return <Text>Error!: ${error.toString()}</Text>; }
 
-  return (
-    <View>
-      {todoListPreviews}
-    </View>
-  );
-};
+      const { allTodos } = data;
+      const todoListPreviews = allTodos.map((list: any) => (
+        <TodoListPreview
+          key={list.id}
+          id={list.id}
+          handleSelect={handleSelect}
+          title={list.title}
+        />
+      ));
+
+      return (
+        <View>
+          {todoListPreviews}
+        </View>
+      );
+    }}
+  </Query>
+);
 
 export default TodoPreviewList;
